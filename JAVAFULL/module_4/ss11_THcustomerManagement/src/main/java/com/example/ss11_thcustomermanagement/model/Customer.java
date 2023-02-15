1c6662e4250e45898b2a4e9334415a764dd39777
Your Name <thuonglx6@gmail.com>
2023-02-15 16:06:05 +0700

    a

--- JAVAFULL/module_4/ss11_THcustomerManagement/src/main/java/com/example/ss11_thcustomermanagement/model/Customer.java
+++ JAVAFULL/module_4/ss11_THcustomerManagement/src/main/java/com/example/ss11_thcustomermanagement/model/Customer.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Customer {
---
+++ JAVAFULL/module_4/ss11_THcustomerManagement/src/main/java/com/example/ss11_thcustomermanagement/model/Test2.java
@@ -0,0 +1,16 @@
+package com.example.ss11_thcustomermanagement.model;
+
+import java.util.List;
+
+public class Test2 {
+//    Tạo đối tượng test1 của class Test1
+    static Integer a;
+
+    public static void main(String[] args) {
+        if (a == 5){
+            System.out.println(a);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
